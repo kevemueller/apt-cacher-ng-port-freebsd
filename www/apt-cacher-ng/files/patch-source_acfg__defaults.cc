f5572fa2691c0648924ae144ccc9b9549946f339
kevemueller <keve.mueller@gmail.com>
2018-02-23 11:04:47 +0100

    portlint changes

--- www/apt-cacher-ng/files/patch-source_acfg__defaults.cc
+++ www/apt-cacher-ng/files/patch-source_acfg__defaults.cc
@@ -1,14 +1,12 @@
-*** source/acfg_defaults.cc.orig	Thu Feb 22 16:53:14 2018
---- source/acfg_defaults.cc	Thu Feb 22 16:53:52 2018
-***************
-*** 39,44 ****
---- 39,47 ----
-      "|/dists/.*/by-hash/.*" // support Debian/Ubuntu by-hash index files
-      "|\\.asc$" // all remaining PGP signatures. Assuming that volatile ones are matched below.
-      "|changelogs/pool/.*/changelog.txt$" // packages.ultimediaos.com
-+     "|(base|doc|games|kernel|lib32|ports|src)\\.txz$" // FreeBSD releases
-+     "|(packagesite|meta)\\.txz?$" // FreeBSD packagesite
-+     "|\\.txz(\\.sig)?$" // FreeBSD package/signature
-  ")$");
-  
-  string svfilepat("/development/rawhide/.*"
+--- source/acfg_defaults.cc.orig	2017-11-05 19:54:13 UTC
++++ source/acfg_defaults.cc
+@@ -39,6 +39,9 @@ string pfilepat(".*(\\.(u|d)?deb|\\.rpm|
+     "|/dists/.*/by-hash/.*" // support Debian/Ubuntu by-hash index files
+     "|\\.asc$" // all remaining PGP signatures. Assuming that volatile ones are matched below.
+     "|changelogs/pool/.*/changelog.txt$" // packages.ultimediaos.com
++    "|(base|doc|games|kernel|lib32|ports|src)\\.txz$" // FreeBSD releases
++    "|(packagesite|meta)\\.txz?$" // FreeBSD packagesite
++    "|\\.txz(\\.sig)?$" // FreeBSD package/signature
+ ")$");
+ 
+ string svfilepat("/development/rawhide/.*"
