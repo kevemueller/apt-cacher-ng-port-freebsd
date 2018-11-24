ae64cbd2fed8dfc8165e2a2aaa36faba4046224e
Keve Mueller <keve@keve.hu>
2018-11-24 19:16:07 +0000

    changes to support upstream version 3.2

--- www/apt-cacher-ng/files/patch-source_acfg__defaults.cc
+++ www/apt-cacher-ng/files/patch-source_acfg__defaults.cc
@@ -1,9 +1,9 @@
---- source/acfg_defaults.cc.orig	2017-11-05 19:54:13 UTC
+--- source/acfg_defaults.cc.orig	2018-09-07 13:02:18 UTC
 +++ source/acfg_defaults.cc
-@@ -39,6 +39,9 @@ string pfilepat(".*(\\.(u|d)?deb|\\.rpm|
-     "|/dists/.*/by-hash/.*" // support Debian/Ubuntu by-hash index files
+@@ -40,6 +40,9 @@ string pfilepat(".*(\\.(u|d)?deb|\\.rpm|
      "|\\.asc$" // all remaining PGP signatures. Assuming that volatile ones are matched below.
      "|changelogs/pool/.*/changelog.txt$" // packages.ultimediaos.com
+     "|/objects/.*/.*\\.(dirtree|filez|commit|commitmeta)|/repo/deltas/.*" // FlatPak
 +    "|(base|doc|games|kernel|lib32|ports|src)\\.txz$" // FreeBSD releases
 +    "|(packagesite|meta)\\.txz?$" // FreeBSD packagesite
 +    "|\\.txz(\\.sig)?$" // FreeBSD package/signature
